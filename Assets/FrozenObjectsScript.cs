1a024e023373aaea0fc56b1cfa4529a310b3c067
Peter Scheel <>
2025-04-05 22:28:31 +0200

    Hit effect on the penguins

--- Assets/FrozenObjectsScript.cs
+++ Assets/FrozenObjectsScript.cs
@@ -2,8 +2,15 @@ using UnityEngine;
 
 public class FrozenObjectsScript : MonoBehaviour
 {
+    public ParticleSystem ParticleSystem;
+    
     void Start()
     {
         GetComponent<Rigidbody>().Sleep();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        ParticleSystem.Play();
     }
 }
