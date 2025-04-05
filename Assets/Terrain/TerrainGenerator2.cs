42a1e980a58a322fcd32bddd60ab74b2b376cbe8
BJORN-VINTHER <g.bvinther@gmail.com>
2025-04-05 18:51:05 +0200

    update generator

--- Assets/Terrain/TerrainGenerator2.cs
+++ Assets/Terrain/TerrainGenerator2.cs
@@ -1,4 +1,7 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer), typeof(MeshCollider))]
 public class TerrainGenerator2 : MonoBehaviour
@@ -9,54 +12,62 @@ public class TerrainGenerator2 : MonoBehaviour
     public int resolution = 50;
     public float length = 50f;
     public float width = 20f;
+    public float edgeWidth = 3f;
+    public float edgeHeight = 10f;
+
+    private const int verticesPerSegment = 4;
 
     [ContextMenu("Generate Slope")]
     private void GenerateRampMesh()
     {
         Mesh mesh = new Mesh();
-        Vector3[] vertices = new Vector3[resolution * 2];
-        Vector2[] uvs = new Vector2[vertices.Length];
-        int[] triangles = new int[(resolution - 1) * 6];
+        var vertices = new List<Vector3>();
+        var triangles = new List<int>();
+        float halfWidth = width / 2f;
 
         for (int i = 0; i < resolution; i++)
         {
             float t = i / (float)(resolution - 1);
-            float z = t * length;
             float y = heightCurve.Evaluate(t) * altitude;
-            float halfWidth = width / 2f;
+            float z = t * length;
 
-            // Left and right vertex
-            vertices[i * 2] = new Vector3(-halfWidth, y, z);  // Left
-            vertices[i * 2 + 1] = new Vector3(halfWidth, y, z); // Right
-
-            // Simple UVs
-            uvs[i * 2] = new Vector2(0, t);
-            uvs[i * 2 + 1] = new Vector2(1, t);
+            // Add vertices
+            var left = i * verticesPerSegment;
+            var right = i * verticesPerSegment + 1;
+            var botLeft = i * verticesPerSegment + 2;
+            var botRight = i * verticesPerSegment + 3;
+            vertices.Add(new Vector3(-halfWidth, y, z));  // Left
+            vertices.Add(new Vector3(halfWidth, y, z)); // Right
+            vertices.Add(new Vector3(-halfWidth - edgeWidth, y - edgeHeight, z));  // Left bot
+            vertices.Add(new Vector3(halfWidth + edgeWidth, y - edgeHeight, z)); // Right right
 
             // Create triangles
-            if (i < resolution - 1)
+            if (i > 0)
             {
-                int vi = i * 2;
-                int ti = i * 6;
-
-                triangles[ti] = vi;
-                triangles[ti + 1] = vi + 2;
-                triangles[ti + 2] = vi + 1;
-
-                triangles[ti + 3] = vi + 1;
-                triangles[ti + 4] = vi + 2;
-                triangles[ti + 5] = vi + 3;
+                AddQuad(triangles, left, right);
+                AddQuad(triangles, botLeft, left);
+                AddQuad(triangles, right, botRight);
             }
         }
 
-        mesh.vertices = vertices;
-        mesh.triangles = triangles;
-        mesh.uv = uvs;
+        mesh.vertices = vertices.ToArray();
+        mesh.triangles = triangles.ToArray();
         mesh.RecalculateNormals();
 
         GetComponent<MeshFilter>().mesh = mesh;
 
         UpdateMeshCollider(mesh);
+    }
+
+    private void AddQuad(List<int> triangles, int v1, int v2)
+    {
+        triangles.Add(v1);
+        triangles.Add(v2);
+        triangles.Add(v1 - verticesPerSegment);
+
+        triangles.Add(v2);
+        triangles.Add(v2 - verticesPerSegment);
+        triangles.Add(v1 - verticesPerSegment);
     }
 
     private void UpdateMeshCollider(Mesh mesh)
