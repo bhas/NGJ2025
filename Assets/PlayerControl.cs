3e91eda772dda56b62faf12eb887462daa4bc161
BJORN-VINTHER <g.bvinther@gmail.com>
2025-04-06 13:16:53 +0200

    commit stuff

--- Assets/PlayerControl.cs
+++ Assets/PlayerControl.cs
@@ -14,8 +14,8 @@ public class PlayerControl : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Rigidbody.Sleep();
         Rigidbody = this.GetComponent<Rigidbody>();
+        Rigidbody.Sleep();
     }
 
     // Update is called once per frames
