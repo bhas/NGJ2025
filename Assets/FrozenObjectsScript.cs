1a17905e386c92d9b2aca3f916a06f05769650b3
Peter Scheel <>
2025-04-06 14:06:35 +0200

    More sensitive pengiuns

--- Assets/FrozenObjectsScript.cs
+++ Assets/FrozenObjectsScript.cs
@@ -4,15 +4,19 @@ public class FrozenObjectsScript : MonoBehaviour
 {
     public ParticleSystem ParticleSystem;
     private bool HitOnce;
+
+    public Rigidbody Rigidbody;
     
     void Start()
     {
-        GetComponent<Rigidbody>().Sleep();
+        Rigidbody.Sleep();
     }
 
     void OnCollisionEnter(Collision collision)
     {
         if(!HitOnce){
+            var randomExplosion = Random.Range(100f, 200f);
+            Rigidbody.AddExplosionForce(randomExplosion, collision.transform.position, randomExplosion);
             HitOnce = true;
             ParticleSystem.Play();
         }
